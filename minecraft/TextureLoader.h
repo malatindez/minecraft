a8a8312c0dcee8c970a1a71c77cef67e4b61337d
malatindez <axenance@gmail.com>
2020-04-22 22:02:43 +0300

    Create TextureLoader.h

---
+++ minecraft/TextureLoader.h
@@ -0,0 +1,62 @@
+#ifndef TEXTURE_LOADER_H
+#define TEXTURE_LOADER_H
+#include <vector>
+#include "Model.h"
+class TextureLoader {
+public:
+	struct Texture {
+		uint32_t id;
+		std::string path;
+        Texture(uint32_t id, std::string path) {
+            this->id = id; this->path = path;
+        }
+	};
+	std::vector<Texture> textures;
+	TextureLoader() {}
+	~TextureLoader() {
+		for (int i = 0; i < textures.size(); i++) {
+			glDeleteTextures(1, &textures[i].id);
+		}
+	}
+	unsigned int LoadTexture(std::string path) {
+        for (int i = 0; i < textures.size(); i++) {
+            if (textures[i].path == path) {
+                return textures[i].id;
+            }
+        }
+        unsigned int textureID;
+        glGenTextures(1, &textureID);
+
+        int width, height, nrComponents;
+        unsigned char* data = stbi_load(path.c_str(), &width, &height, &nrComponents, 0);
+        if (data) {
+            GLenum format = GL_RED;
+            if (nrComponents == 1)
+                format = GL_RED;
+            else if (nrComponents == 3)
+                format = GL_RGB;
+            else if (nrComponents == 4)
+                format = GL_RGBA;
+
+            glBindTexture(GL_TEXTURE_2D, textureID);
+            glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+            glGenerateMipmap(GL_TEXTURE_2D);
+
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_NEAREST);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+            stbi_image_free(data);
+        }
+        else {
+            std::cout << "Texture failed to load at path: " << path << std::endl;
+            stbi_image_free(data);
+        }
+
+        textures.push_back(Texture(textureID, path));
+        return textureID;
+	}
+};
+
+#endif
