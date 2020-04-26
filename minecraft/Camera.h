0d4117c2006edd8953ab83d079e6dc21397bc97b
malatindez <axenance@gmail.com>
2020-04-27 01:27:07 +0300

    Update Camera.h

--- minecraft/Camera.h
+++ minecraft/Camera.h
@@ -7,16 +7,6 @@
 #include <algorithm>
 #include <vector>
 
-// Defines several possible options for camera movement. Used as abstraction to stay away from window-system specific input methods
-enum Camera_Movement {
-    FORWARD,
-    BACKWARD,
-    LEFT,
-    RIGHT,
-    UP,
-    DOWN
-};
-
 // Default camera values
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
@@ -26,9 +16,16 @@ const float ZOOM = 90.0f;
 
 
 // An abstract camera class that processes input and calculates the corresponding Euler Angles, Vectors and Matrices for use in OpenGL
-class Camera
-{
+class Camera {
 public:
+    enum class Movement {
+        FORWARD = 0,
+        BACKWARD = 1,
+        LEFT = 2,
+        RIGHT = 3,
+        UP = 4,
+        DOWN = 5
+    };
     // Camera Attributes
     glm::vec3 Position;
     glm::vec3 Front;
@@ -51,7 +48,7 @@ public:
     glm::mat4 GetViewMatrix();
 
     // Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
-    void ProcessKeyboard(Camera_Movement direction, float deltaTime);
+    void ProcessKeyboard(Movement direction, float deltaTime);
 
     // Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
     void ProcessMouseMovement(double xoffset, double yoffset, GLboolean constrainPitch = true);
