04099614d8811857aacb5a7386804a9e65eda59b
malatindez <axenance@gmail.com>
2020-04-28 13:58:16 +0300

    Update Camera.h

--- minecraft/Camera.h
+++ minecraft/Camera.h
@@ -10,7 +10,7 @@
 // Default camera values
 const float YAW = -90.0f;
 const float PITCH = 0.0f;
-const float SPEED = 8.0f;
+const float SPEED = 80.0f;
 const float SENSITIVITY = 0.1f;
 const float ZOOM = 90.0f;
 
