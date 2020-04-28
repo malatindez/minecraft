436fbacf83c8b99f1b344abbd1210cdac3c95316
malatindez <axenance@gmail.com>
2020-04-28 13:58:23 +0300

    Update Interface.h

--- minecraft/Interface.h
+++ minecraft/Interface.h
@@ -1,6 +1,6 @@
 #ifndef INTERFACE_H
 #define INTERFACE_H
-#include <glfw3.h>
+#include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
