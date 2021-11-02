df8952a9e592f3bd97db5991c63fbf91d606755f
malatindez <53783400+malatindez@users.noreply.github.com>
2021-11-02 15:04:24 +0200

    reformat code

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,85 +1,92 @@
 #include "Camera.h"
 Camera::Camera(glm::vec3 position, glm::vec3 up, float yaw, float pitch)
-                : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
-{
-    Position = position;
-    WorldUp = up;
-    Yaw = yaw;
-    Pitch = pitch;
-    updateCameraVectors();
+    : Front(glm::vec3(0.0f, 0.0f, -1.0f)),
+      MovementSpeed(SPEED),
+      MouseSensitivity(SENSITIVITY),
+      Zoom(ZOOM) {
+  Position = position;
+  WorldUp = up;
+  Yaw = yaw;
+  Pitch = pitch;
+  updateCameraVectors();
 }
 // Constructor with scalar values
-Camera::Camera(float posX, float posY, float posZ, float upX, float upY, float upZ, float yaw, float pitch) 
-                : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(SPEED), MouseSensitivity(SENSITIVITY), Zoom(ZOOM)
-{
-    Position = glm::vec3(posX, posY, posZ);
-    WorldUp = glm::vec3(upX, upY, upZ);
-    Yaw = yaw;
-    Pitch = pitch;
-    updateCameraVectors();
+Camera::Camera(float posX, float posY, float posZ, float upX, float upY,
+               float upZ, float yaw, float pitch)
+    : Front(glm::vec3(0.0f, 0.0f, -1.0f)),
+      MovementSpeed(SPEED),
+      MouseSensitivity(SENSITIVITY),
+      Zoom(ZOOM) {
+  Position = glm::vec3(posX, posY, posZ);
+  WorldUp = glm::vec3(upX, upY, upZ);
+  Yaw = yaw;
+  Pitch = pitch;
+  updateCameraVectors();
 }
 
 // Returns the view matrix calculated using Euler Angles and the LookAt Matrix
 glm::mat4 Camera::GetViewMatrix() {
-    return glm::lookAt(Position, Position + Front, Up);
+  return glm::lookAt(Position, Position + Front, Up);
 }
 
-// Processes input received from any keyboard-like input system. Accepts input parameter in the form of camera defined ENUM (to abstract it from windowing systems)
+// Processes input received from any keyboard-like input system. Accepts input
+// parameter in the form of camera defined ENUM (to abstract it from windowing
+// systems)
 void Camera::ProcessKeyboard(Movement direction, float deltaTime) {
-    auto prevPosition = Position;
-    float velocity = (float)MovementSpeed * deltaTime;
-    if (direction == Movement::FORWARD)
-        Position += glm::vec3(Front.x / cos(glm::radians(Pitch)), 0, Front.z / cos(glm::radians(Pitch))) * velocity;
-    if (direction == Movement::BACKWARD)
-        Position -= glm::vec3(Front.x / cos(glm::radians(Pitch)), 0, Front.z / cos(glm::radians(Pitch))) * velocity;
-    if (direction == Movement::LEFT)
-        Position -= Right * velocity;
-    if (direction == Movement::RIGHT)
-        Position += Right * velocity;
-    if (direction == Movement::DOWN)
-        Position -= glm::vec3(0, 1, 0) * velocity;
-    if (direction == Movement::UP)
-        Position += glm::vec3(0, 1, 0) * velocity;
-
+  auto prevPosition = Position;
+  float velocity = (float)MovementSpeed * deltaTime;
+  if (direction == Movement::FORWARD)
+    Position += glm::vec3(Front.x / cos(glm::radians(Pitch)), 0,
+                          Front.z / cos(glm::radians(Pitch))) *
+                velocity;
+  if (direction == Movement::BACKWARD)
+    Position -= glm::vec3(Front.x / cos(glm::radians(Pitch)), 0,
+                          Front.z / cos(glm::radians(Pitch))) *
+                velocity;
+  if (direction == Movement::LEFT) Position -= Right * velocity;
+  if (direction == Movement::RIGHT) Position += Right * velocity;
+  if (direction == Movement::DOWN) Position -= glm::vec3(0, 1, 0) * velocity;
+  if (direction == Movement::UP) Position += glm::vec3(0, 1, 0) * velocity;
 }
 
-// Processes input received from a mouse input system. Expects the offset value in both the x and y direction.
-void Camera::ProcessMouseMovement(double xoffset, double yoffset, GLboolean constrainPitch) {
-    xoffset *= MouseSensitivity * Zoom / ZOOM;
-    yoffset *= MouseSensitivity * Zoom / ZOOM;
+// Processes input received from a mouse input system. Expects the offset value
+// in both the x and y direction.
+void Camera::ProcessMouseMovement(double xoffset, double yoffset,
+                                  GLboolean constrainPitch) {
+  xoffset *= MouseSensitivity * Zoom / ZOOM;
+  yoffset *= MouseSensitivity * Zoom / ZOOM;
 
-    Yaw += xoffset;
-    Pitch += yoffset;
+  Yaw += xoffset;
+  Pitch += yoffset;
 
-    // Make sure that when pitch is out of bounds, screen doesn't get flipped
-    if (constrainPitch) {
-        if (Pitch > 89.9f)
-            Pitch = 89.9f;
-        if (Pitch < -89.9f)
-            Pitch = -89.9f;
-    }
+  // Make sure that when pitch is out of bounds, screen doesn't get flipped
+  if (constrainPitch) {
+    if (Pitch > 89.9f) Pitch = 89.9f;
+    if (Pitch < -89.9f) Pitch = -89.9f;
+  }
 
-    // Update Front, Right and Up Vectors using the updated Euler angles
-    updateCameraVectors();
+  // Update Front, Right and Up Vectors using the updated Euler angles
+  updateCameraVectors();
 }
 
-// Processes input received from a mouse scroll-wheel event. Only requires input on the vertical wheel-axis
+// Processes input received from a mouse scroll-wheel event. Only requires input
+// on the vertical wheel-axis
 void Camera::ProcessMouseScroll(float yoffset) {
-    if (Zoom >= 1.0f && Zoom <= ZOOM)
-        Zoom -= yoffset;
-    if (Zoom <= 1.0f)
-        Zoom = 1.0f;
-    if (Zoom >= ZOOM)
-        Zoom = ZOOM;
+  if (Zoom >= 1.0f && Zoom <= ZOOM) Zoom -= yoffset;
+  if (Zoom <= 1.0f) Zoom = 1.0f;
+  if (Zoom >= ZOOM) Zoom = ZOOM;
 }
 void Camera::updateCameraVectors() {
-    // Calculate the new Front vector
-    glm::vec3 front;
-    front.x = cos(glm::radians((float)Yaw)) * cos(glm::radians((float)Pitch));
-    front.y = sin(glm::radians((float)Pitch));
-    front.z = sin(glm::radians((float)Yaw)) * cos(glm::radians((float)Pitch));
-    Front = glm::normalize(front);
-    // Also re-calculate the Right and Up vector
-    Right = glm::normalize(glm::cross(Front, WorldUp));  // Normalize the vectors, because their length gets closer to 0 the more you look up or down which results in slower movement.
-    Up = glm::normalize(glm::cross(Right, Front));
+  // Calculate the new Front vector
+  glm::vec3 front;
+  front.x = cos(glm::radians((float)Yaw)) * cos(glm::radians((float)Pitch));
+  front.y = sin(glm::radians((float)Pitch));
+  front.z = sin(glm::radians((float)Yaw)) * cos(glm::radians((float)Pitch));
+  Front = glm::normalize(front);
+  // Also re-calculate the Right and Up vector
+  Right = glm::normalize(glm::cross(
+      Front, WorldUp));  // Normalize the vectors, because their length gets
+                         // closer to 0 the more you look up or down which
+                         // results in slower movement.
+  Up = glm::normalize(glm::cross(Right, Front));
 }
--- src/Player.h
+++ src/Player.h
@@ -1,108 +1,114 @@
 #ifndef PLAYER_H
 #define PLAYER_H
+#include <GLFW/glfw3.h>
+
 #include "Camera.h"
 #include "World.h"
-#include <GLFW/glfw3.h>
 class Player {
-    GLFWwindow* window = nullptr;
-    World* world = nullptr;
-    Input *input;
-    KeySequence *forward, *backward, *left, *right, *up, *down, *breakSeq, *placeSeq;
-    double* deltaTime; // pointer to location where is deltatime updating
-public:
-    Camera cam;
-    Player(GLFWwindow* window, World *world, double* deltaTimeLocation, Input* input) {
-        this->input = input;
-        this->deltaTime = deltaTimeLocation;
-        this->window = window;
-        this->world = world;
-        input->addCheckingKey(GLFW_KEY_W);
-        input->addCheckingKey(GLFW_KEY_A);
-        input->addCheckingKey(GLFW_KEY_S);
-        input->addCheckingKey(GLFW_KEY_D);
-        input->addCheckingKey(GLFW_KEY_SPACE);
-        input->addCheckingKey(GLFW_KEY_LEFT_CONTROL);
-        input->addCheckingKey(GLFW_MOUSE_BUTTON_LEFT);
-        input->addCheckingKey(GLFW_MOUSE_BUTTON_RIGHT);
-        forward = new KeySequence(GLFW_KEY_W);
-        backward = new  KeySequence(GLFW_KEY_S);
-        left = new KeySequence(GLFW_KEY_A);
-        right = new KeySequence(GLFW_KEY_D);
-        up = new KeySequence(GLFW_KEY_SPACE);
-        down = new KeySequence(GLFW_KEY_LEFT_CONTROL);
-        breakSeq = new KeySequence((uint16_t)GLFW_MOUSE_BUTTON_LEFT);
-        placeSeq = new KeySequence(GLFW_MOUSE_BUTTON_RIGHT);
-    }
-    ~Player() {
-        delete forward;
-        delete backward;
-        delete left;
-        delete right;
-        delete up;
-        delete down;
-        delete breakSeq;
-        delete placeSeq;
-    }
-private:
-    double lastX = 0, lastY = 0;
-    bool firstMouse = true;
-    void Breaking() {
-        //breaking = true;
-    }
-    void Placing() {
-        
-    }
-public:
-    int32_t hoveredx = 2147483647, hoveredy = 2147483647, hoveredz = 2147483647;
-    uint16_t breakingStage;
-    void update() {
-        if (input->checkSequence((*forward))) {
-            cam.ProcessKeyboard(Camera::Movement::FORWARD, (*deltaTime));
-        }
-        if (input->checkSequence((*backward))) {
-            cam.ProcessKeyboard(Camera::Movement::BACKWARD, (*deltaTime));
-        }
-        if (input->checkSequence((*left))) {
-            cam.ProcessKeyboard(Camera::Movement::LEFT, (*deltaTime));
-        }
-        if (input->checkSequence((*right))) {
-            cam.ProcessKeyboard(Camera::Movement::RIGHT, (*deltaTime));
-        }
-        if (input->checkSequence((*up))) {
-            cam.ProcessKeyboard(Camera::Movement::UP, (*deltaTime));
-        }
-        if (input->checkSequence((*down))) {
-            cam.ProcessKeyboard(Camera::Movement::DOWN, (*deltaTime));
-        }
-        if (input->checkSequence((*breakSeq))) {
-            Breaking();
-        }
-        if (input->checkSequence((*placeSeq))) {
-            Placing();
-        }
-        std::pair<double, double> pos = input->getMousePosition();
-        
-        if (firstMouse) {
-            lastX = pos.first;
-            lastY = pos.second;
-            firstMouse = false;
-        }
+  GLFWwindow* window = nullptr;
+  World* world = nullptr;
+  Input* input;
+  KeySequence *forward, *backward, *left, *right, *up, *down, *breakSeq,
+      *placeSeq;
+  double* deltaTime;  // pointer to location where is deltatime updating
+ public:
+  Camera cam;
+  Player(GLFWwindow* window, World* world, double* deltaTimeLocation,
+         Input* input) {
+    this->input = input;
+    this->deltaTime = deltaTimeLocation;
+    this->window = window;
+    this->world = world;
+    input->addCheckingKey(GLFW_KEY_W);
+    input->addCheckingKey(GLFW_KEY_A);
+    input->addCheckingKey(GLFW_KEY_S);
+    input->addCheckingKey(GLFW_KEY_D);
+    input->addCheckingKey(GLFW_KEY_SPACE);
+    input->addCheckingKey(GLFW_KEY_LEFT_CONTROL);
+    input->addCheckingKey(GLFW_MOUSE_BUTTON_LEFT);
+    input->addCheckingKey(GLFW_MOUSE_BUTTON_RIGHT);
+    forward = new KeySequence(GLFW_KEY_W);
+    backward = new KeySequence(GLFW_KEY_S);
+    left = new KeySequence(GLFW_KEY_A);
+    right = new KeySequence(GLFW_KEY_D);
+    up = new KeySequence(GLFW_KEY_SPACE);
+    down = new KeySequence(GLFW_KEY_LEFT_CONTROL);
+    breakSeq = new KeySequence((uint16_t)GLFW_MOUSE_BUTTON_LEFT);
+    placeSeq = new KeySequence(GLFW_MOUSE_BUTTON_RIGHT);
+  }
+  ~Player() {
+    delete forward;
+    delete backward;
+    delete left;
+    delete right;
+    delete up;
+    delete down;
+    delete breakSeq;
+    delete placeSeq;
+  }
 
-        double xoffset = pos.first - lastX;
-        double yoffset = lastY - pos.second; // reversed since y-coordinates go from bottom to top
+ private:
+  double lastX = 0, lastY = 0;
+  bool firstMouse = true;
+  void Breaking() {
+    // breaking = true;
+  }
+  void Placing() {}
 
-        lastX = pos.first;
-        lastY = pos.second;
-
-        cam.ProcessMouseMovement(xoffset, yoffset);
-
-        for (double i = 0; i < 5; i += 0.01) {
-            glm::vec3 g = cam.Position + glm::vec3(cam.Front.x * i, cam.Front.y * i, cam.Front.z * i);
-            int32_t x = (int32_t)std::round(g.x);
-            int32_t y = (int32_t)std::round(g.y);
-            int32_t z = (int32_t)std::round(g.z);
-        }
+ public:
+  int32_t hoveredx = 2147483647, hoveredy = 2147483647, hoveredz = 2147483647;
+  uint16_t breakingStage;
+  void update() {
+    if (input->checkSequence((*forward))) {
+      cam.ProcessKeyboard(Camera::Movement::FORWARD, (*deltaTime));
     }
+    if (input->checkSequence((*backward))) {
+      cam.ProcessKeyboard(Camera::Movement::BACKWARD, (*deltaTime));
+    }
+    if (input->checkSequence((*left))) {
+      cam.ProcessKeyboard(Camera::Movement::LEFT, (*deltaTime));
+    }
+    if (input->checkSequence((*right))) {
+      cam.ProcessKeyboard(Camera::Movement::RIGHT, (*deltaTime));
+    }
+    if (input->checkSequence((*up))) {
+      cam.ProcessKeyboard(Camera::Movement::UP, (*deltaTime));
+    }
+    if (input->checkSequence((*down))) {
+      cam.ProcessKeyboard(Camera::Movement::DOWN, (*deltaTime));
+    }
+    if (input->checkSequence((*breakSeq))) {
+      Breaking();
+    }
+    if (input->checkSequence((*placeSeq))) {
+      Placing();
+    }
+    std::pair<double, double> pos = input->getMousePosition();
+
+    if (firstMouse) {
+      lastX = pos.first;
+      lastY = pos.second;
+      firstMouse = false;
+    }
+
+    double xoffset = pos.first - lastX;
+    double yoffset =
+        lastY -
+        pos.second;  // reversed since y-coordinates go from bottom to top
+
+    lastX = pos.first;
+    lastY = pos.second;
+
+    cam.ProcessMouseMovement(xoffset, yoffset);
+
+    for (double i = 0; i < 5; i += 0.01) {
+      glm::vec3 g = cam.Position + glm::vec3(cam.Front.x * i, cam.Front.y * i,
+                                             cam.Front.z * i);
+      int32_t x = (int32_t)std::round(g.x);
+      int32_t y = (int32_t)std::round(g.y);
+      int32_t z = (int32_t)std::round(g.z);
+    }
+  }
 };
 
 #endif
