cbe2f5d640aa7309e562b0ae0c7d940913e31469
malatindez <53783400+malatindez@users.noreply.github.com>
2021-12-07 09:28:06 +0200

    Create Utils for tests & move functions from TestResources there

--- src/tests/TestResources.cpp
+++ src/tests/TestResources.cpp
@@ -1,96 +1,10 @@
-#define minecraft_RESOURCE_PACKING
-#include <algorithm>
-#include <chrono>
-#include <filesystem>
-#include <random>
 
+#define minecraft_RESOURCE_PACKING
 #include "Resources/Resources.hpp"
 #include "Resources/pack.hpp"
+#include "Utils.hpp"
 #include "gtest/gtest.h"
 namespace fs = std::filesystem;
-
-static std::random_device rd;
-static std::mt19937 gen(rd());
-
-[[nodiscard]] static inline std::string Uint32ToUTF8(uint32_t const& value) {
-  if (value > 0x10FFFF) {
-    throw std::invalid_argument("Invalid value provided.");
-  }
-
-  static const std::vector<uint32_t> kUnicodeThresholds = {0x80, 0x800, 0x10000,
-                                                           UINT32_MAX};
-
-  std::string return_value = "";
-
-  if (value < 0x80) {
-    return_value += (char)value;
-  } else if (value < 0x800) {
-    return_value += (char)((value >> 0x06) & 0b00011111) | 0b11000000;
-    return_value += (char)((value >> 0x00) & 0b00111111) | 0b10000000;
-  } else if (value < 0x10000) {
-    return_value += (char)((value >> 0x0C) & 0b00001111) | 0b11100000;
-    return_value += (char)((value >> 0x06) & 0b00111111) | 0b10000000;
-    return_value += (char)((value >> 0x00) & 0b00111111) | 0b10000000;
-  } else {
-    return_value += (char)((value >> 0x12) & 0b00000111) | 0b11110000;
-    return_value += (char)((value >> 0x0C) & 0b00111111) | 0b10000000;
-    return_value += (char)((value >> 0x06) & 0b00111111) | 0b10000000;
-    return_value += (char)((value >> 0x00) & 0b00111111) | 0b10000000;
-  }
-  return return_value;
-}
-
-[[nodiscard]] static inline std::string RandomUTF8Char(
-    uint32_t const& from = 0, uint32_t const& to = 0xA000) {
-  static const std::uniform_int_distribution<uint32_t> dis(from, to);
-  return Uint32ToUTF8(dis(gen));
-}
-
-[[nodiscard]] static std::string RandomUTF8String(size_t const& size = 32) {
-  std::string return_value;
-  for (size_t i = 0; i < size; i++) {
-    return_value += RandomUTF8Char();
-  }
-  return return_value;
-}
-[[nodiscard]] static std::string RandomUTF8Filename(size_t const& size = 32) {
-  static const std::string kProhibitedCharacters = "<>:\"/\\|?*";
-  std::string return_value;
-  for (size_t i = 0; i < size - 1;) {
-    std::string temp = RandomUTF8Char(32);
-    if (temp.size() == 1 && (kProhibitedCharacters.end() !=
-                             std::find(kProhibitedCharacters.begin(),
-                                       kProhibitedCharacters.end(), temp[0]))) {
-      continue;
-    }
-    return_value += temp;
-    i++;
-  }
-  std::string
-      final_char;  // files in windows cannot end with space nor with dot
-  do {
-    final_char = RandomUTF8Char(33);
-  } while (final_char.size() == 1 &&
-           (kProhibitedCharacters.end() !=
-                std::find(kProhibitedCharacters.begin(),
-                          kProhibitedCharacters.end(), final_char[0]) ||
-            final_char[0] == '.'));
-  return return_value + final_char;
-}
-
-static void CreateFile(fs::path const& path, char const* data,
-                       const size_t size) {
-  fs::create_directories({path.parent_path()});
-  std::ofstream ofs(path);
-  ofs.write(data, size);
-  ofs.close();
-}
-
-static inline double time_diff(
-    std::chrono::high_resolution_clock::time_point begin,
-    std::chrono::high_resolution_clock::time_point end) {
-  return std::chrono::duration<double, std::milli>(end - begin).count();
-}
 
 static const std::vector<std::string> kFixedTestfiles = {"a/b.txt", "a/b/c.txt",
                                                          "a/b/c/d.txt"};
@@ -98,20 +12,21 @@ static const std::vector<std::string> kFixedTestfiles = {"a/b.txt", "a/b/c.txt",
 class TestResources : public ::testing::Test {
  protected:
   static void SetUpTestSuite() {
-    static const std::uniform_int_distribution<uint32_t> kAmountOfFiles{28, 34};
-    static const std::uniform_int_distribution<uint32_t> kStringSize{16, 24};
-    static const std::uniform_int_distribution<uint32_t> kFileSize{10000,
-                                                                   100000};
+    static const std::pair<uint32_t, uint32_t> kAmountOfFiles{28, 34};
+    static const std::pair<uint32_t, uint32_t> kStringSize{16, 24};
+    static const std::pair<uint32_t, uint32_t> kFileSize{10000, 100000};
     packing_succeded = false;
     dir_ = fs::temp_directory_path() / "minecraft_test/TestResources/";
     for (std::string const& path : kFixedTestfiles) {
       CreateFile(dir_ / path, path.c_str(), path.size());
     }
 
-    for (uint32_t i = 0; i < kAmountOfFiles(gen); i++) {
-      std::filesystem::path name = RandomUTF8Filename(kStringSize(gen));
+    for (uint32_t i = 0;
+         i < RandomUint32(kAmountOfFiles.first, kAmountOfFiles.second); i++) {
+      std::filesystem::path name = RandomUTF8Filename(
+          RandomUint32(kStringSize.first, kStringSize.second));
       auto path = dir_ / "unicode_test/" / name;
-      std::vector<char> t(kFileSize(gen));
+      std::vector<char> t(RandomUint32(kFileSize.first, kFileSize.second));
       CreateFile(path, t.data(), t.size());
       unicode_files_.push_back(std::filesystem::path("unicode_test") / name);
     }
---
+++ src/tests/Utils.hpp
@@ -0,0 +1,102 @@
+#pragma once
+#include <algorithm>
+#include <chrono>
+#include <filesystem>
+#include <fstream>
+#include <limits>
+#include <random>
+
+#include "Utils/Utils.hpp"
+
+namespace fs = std::filesystem;
+
+[[nodiscard]] constexpr std::string ExcludeString(std::string const& a,
+                                                  std::string const& b) {
+  std::string return_value{a};
+  std::erase_if(return_value, [&b](char const& c) {
+    return std::find(b.begin(), b.end(), c) != b.end();
+  });
+  return return_value;
+}
+
+[[nodiscard]] std::string Uint32ToUTF8(uint32_t const& value);
+
+[[nodiscard]] inline std::string RandomUTF8Char(uint32_t const& from = 0,
+                                                uint32_t const& to = 0xA000);
+
+[[nodiscard]] std::string RandomUTF8String(size_t const& size = 32);
+[[nodiscard]] std::string RandomUTF8Filename(size_t const& size = 32);
+
+const std::string kAsciiCharacters =
+    "' "
+    "!\"#$%&'()*+,-./"
+    "0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`"
+    "abcdefghijklmnopqrstuvwxyz{|}~";
+
+constexpr std::string AllBinaryCharacters() {
+  std::string return_value;
+  for (char i = -128; i < 127; i++) {
+    return_value.push_back(i);
+  }
+  return return_value;
+};
+const std::string kAllBinaryCharacters = AllBinaryCharacters();
+
+[[nodiscard]] std::string RandomBinaryString(size_t const& size = 32);
+
+[[nodiscard]] std::string RandomString(
+    size_t const& size = 32, std::string including = kAsciiCharacters);
+
+[[nodiscard]] uint64_t RandomUint64(
+    uint64_t min = std::numeric_limits<uint64_t>::min(),
+    uint64_t max = std::numeric_limits<uint64_t>::max());
+[[nodiscard]] uint32_t RandomUint32(
+    uint32_t min = std::numeric_limits<uint32_t>::min(),
+    uint32_t max = std::numeric_limits<uint32_t>::max());
+[[nodiscard]] uint32_t RandomUint16(
+    uint16_t min = std::numeric_limits<uint16_t>::min(),
+    uint16_t max = std::numeric_limits<uint16_t>::max());
+[[nodiscard]] int64_t RandomInt64(
+    int64_t min = std::numeric_limits<int64_t>::min(),
+    int64_t max = std::numeric_limits<int64_t>::max());
+[[nodiscard]] int32_t RandomInt32(
+    int32_t min = std::numeric_limits<int32_t>::min(),
+    int32_t max = std::numeric_limits<int32_t>::max());
+[[nodiscard]] int16_t RandomInt16(
+    int16_t min = std::numeric_limits<int16_t>::min(),
+    int16_t max = std::numeric_limits<int16_t>::max());
+
+[[nodiscard]] int64_t RandomInteger(
+    int64_t min = std::numeric_limits<int64_t>::min(),
+    int64_t max = std::numeric_limits<int64_t>::max());
+
+[[nodiscard]] uint64_t RandomUnsignedInteger(
+    uint64_t min = std::numeric_limits<uint64_t>::min(),
+    uint64_t max = std::numeric_limits<uint64_t>::max());
+
+[[nodiscard]] size_t RandomSizeT(
+    size_t min = std::numeric_limits<size_t>::min(),
+    size_t max = std::numeric_limits<size_t>::max());
+
+[[nodiscard]] char RandomChar(char min = std::numeric_limits<char>::min(),
+                              char max = std::numeric_limits<char>::max());
+
+[[nodiscard]] float RandomFloat(float min = std::numeric_limits<float>::min(),
+                                float max = std::numeric_limits<float>::max());
+
+[[nodiscard]] double RandomDouble(
+    double min = std::numeric_limits<double>::min(),
+    double max = std::numeric_limits<double>::max());
+
+[[nodiscard]] double RandomLongDouble(
+    long double min = std::numeric_limits<long double>::min(),
+    long double max = std::numeric_limits<long double>::max());
+
+[[nodiscard]] std::string RandomFilename(size_t const& size = 32);
+void CreateFile(fs::path const& path, char const* data, const size_t size);
+
+[[nodiscard]] constexpr double time_diff(
+    std::chrono::high_resolution_clock::time_point begin,
+    std::chrono::high_resolution_clock::time_point end) {
+  return std::chrono::duration<double, std::milli>(end - begin).count();
+}
